eca4da46e798aba626c922f9abaa16349c959feb
Mo Binni <mo.binni@icapps.com>
2014-11-06 21:45:47 +0100

    fixed bower

--- app/scripts/datepicker.coffee
+++ app/scripts/datepicker.coffee
@@ -8,10 +8,29 @@
  #
  # Main module of the application.
 ###
-angular
+app = angular
   .module('materialDatePicker', [
     'mgcrea.ngStrap',
     'ngMaterial',
     'ui.utils'
   ])
 
+app.directive('mdDatePicker', ['$datepicker', ($datepicker)->
+  template: '<div class="md-toolbar-tools dark">' +
+    ' <md-input-group ng-disabled="isDisabled">' +
+    '     <label>{{label}}</label>' +
+    '    <md-input id="date-picker" model-view-value="true"' +
+    '             ng-model="value" bs-datepicker></md-input>' +
+    '</md-input-group>' +
+    '</div>'
+  restrict: 'E'
+  scope: {
+    lineColor: '@'
+    textColor: '@'
+    mask: '@'
+  }
+  link: (scope, element, attrs) ->
+    if scope.textColor
+      $('#date-picker').attr('style', 'color:' + scope.textColor + '!important')
+    return;
+])
--- build/datepicker.js
+++ build/datepicker.js
@@ -9,6 +9,27 @@
     *
     * Main module of the application.
    */
-  angular.module('materialDatePicker', ['mgcrea.ngStrap', 'ngMaterial', 'ui.utils']);
+  var app;
+
+  app = angular.module('materialDatePicker', ['mgcrea.ngStrap', 'ngMaterial', 'ui.utils']);
+
+  app.directive('mdDatePicker', [
+    '$datepicker', function($datepicker) {
+      return {
+        template: '<div class="md-toolbar-tools dark">' + ' <md-input-group ng-disabled="isDisabled">' + '     <label>{{label}}</label>' + '    <md-input id="date-picker" model-view-value="true"' + '             ng-model="value" bs-datepicker></md-input>' + '</md-input-group>' + '</div>',
+        restrict: 'E',
+        scope: {
+          lineColor: '@',
+          textColor: '@',
+          mask: '@'
+        },
+        link: function(scope, element, attrs) {
+          if (scope.textColor) {
+            $('#date-picker').attr('style', 'color:' + scope.textColor + '!important');
+          }
+        }
+      };
+    }
+  ]);
 
 }).call(this);
