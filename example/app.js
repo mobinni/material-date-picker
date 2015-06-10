cf3d106a928714e50cb706c6aa076b23cc3d83cc
Mo Binni <mo.binni@hotmail.com>
2015-06-10 20:04:11 +0200

    added example back

---
+++ example/app.js
@@ -0,0 +1,28 @@
+/**
+ * Created by mobinni on 22/04/15.
+ */
+var app = angular.module('exampleApp', [
+    'materialDatePicker'
+]);
+
+app.controller('MainCtrl', function ($scope) {
+    $scope.arrows = {
+        year: {
+            left: 'images/white_arrow_left.svg',
+            right: 'images/white_arrow_right.svg'
+        },
+        month: {
+            left: 'images/grey_arrow_left.svg',
+            right: 'images/grey_arrow_right.svg'
+        }
+    }
+    $scope.header = {
+        monday: 'Mon',
+        tuesday: 'Tue',
+        wednesday: 'Wed',
+        thursday: 'Thu',
+        friday: 'Fri',
+        saturday: 'Sat',
+        sunday: 'Sun',
+    }
+})
